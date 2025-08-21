b1b79fb353e3593219c2798c7c0f975285ef93d3
ochoareyesjosue-spec <ochoareyesjosue@gmail.com>
2025-08-21 00:03:05 -0500

    Delete reto22.py

--- reto22.py
+++
@@ -1,9 +0,0 @@
-def contar_palabras(texto):
-    palabras = texto.split()  
-    return len(palabras)
-    
-    
-print(contar_palabras("hola mundo"))      # 2
-print(contar_palabras(""))                # 0
-print(contar_palabras("   hola   mundo")) # 2
-print(contar_palabras("solo"))            # 1
